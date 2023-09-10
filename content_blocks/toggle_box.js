9113c7bca43ffb3240ebe0f948600428873bd280
Viet Nguyen <vietnguyen168@gmail.com>
2023-09-10 11:37:48 +0700

    add spine section and jquery toggle

--- content_blocks/toggle_box.js
+++ content_blocks/toggle_box.js
@@ -7,6 +7,9 @@ export default class ToggleBox extends TitledBlock
         super(p_name);
         this._speed = 0;
         this.get_title_obj().getDOM().addEventListener("click", this._toggle.bind(this));
+        this.get_spine_obj().getDOM().addEventListener("click", this._toggle.bind(this));
+        $(this.get_text_obj().getDOM()).hide();
+        this._process_spine_bg();
     }
 
     set_speed(spd)
@@ -18,5 +21,46 @@ export default class ToggleBox extends TitledBlock
     {
         let hidden_space = this.get_text_obj().getDOM();
         $(hidden_space).toggle(this._speed);
+        this._draw_folding();
+        this._process_spine_bg();
+    }
+
+    _draw_folding()
+    {
+        let spine = this.get_spine_obj().getDOM();
+    }
+    
+    _is_visible()
+    {
+        let hidden_space = this.get_text_obj().getDOM();
+        return $(hidden_space).is(':visible');
+    }
+
+    _process_spine_bg()
+    {
+        let spine_bg = document.createElement('div');
+        spine_bg.classList.add("spine_bg");
+        let spine = this.get_spine_obj().getDOM();
+        let existing_bg = spine.getElementsByClassName("spine_bg");
+        for (let bg of existing_bg)
+        {
+            spine.removeChild(bg);
+        }
+        
+        if (this._is_visible()){
+            let spine_x = window.getComputedStyle(this.get_spine_obj().getDOM()).width;
+            let spine_y = window.getComputedStyle(this.get_spine_obj().getDOM()).height;
+            spine_bg.style.fontSize = parseInt(spine_x) + "px";
+            spine_bg.style.marginTop = parseInt(spine_y)/2 - parseInt(spine_x)/2  + "px";
+            spine_bg.style.marginTop = parseInt(spine_y)/2 - parseInt(spine_x)/2  + "px";
+            spine_bg.innerHTML = "&minus;";
+        }
+        else{
+            spine_bg.style.marginTop = "0px";
+            spine_bg.innerHTML = "&#10010;";
+        }
+
+            
+        spine.appendChild(spine_bg);
     }
 }
